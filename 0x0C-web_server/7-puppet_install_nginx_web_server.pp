1480b6d3053b7cef8302ab6fe371bac37a8a263f
Michael Aroworade <Ayoaro85@gmail.com>
2024-03-27 03:02:13 +0100

    install nginx server using puppet.

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,29 @@
+# Installs a Nginx server
+
+exec {'install':
+  provider => shell,
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  echo "Hello World!" > /etc/nginx/html/index.html';
+  touch /etc/nginx/html/404.html
+  echo "Ceci n'est pas une page" > /etc/nginx/html/404.html
+  
+  printf %s "server {
+    listen 80;
+    listen [::]:80 default_server;
+    root   /etc/nginx/html;
+    index  index.html index.htm;
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /etc/nginx/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart,
+}
